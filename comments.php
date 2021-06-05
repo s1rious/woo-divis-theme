3f5086da7bd7891e3ca7489edcb652900de3588d
mginx <sirioustop03@gmail.com>
2021-06-06 00:38:38 +0300

    1.0.4

--- comments.php
+++ comments.php
@@ -10,6 +10,8 @@ if (post_password_required()) {
 
 ?>
     <?php
+
+        // Do not delete these lines
         if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
         die ('Please do not load this page directly. Thanks!');
         ?>
